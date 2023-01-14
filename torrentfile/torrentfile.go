aa7eb25ff9e6f86a9dcefc3be965f9a80950baf1
pchchv <ipchchv@gmail.com>
2023-01-14 18:58:50 +0300

    Implement a DownloadToFile method

--- torrentfile/torrentfile.go
+++ torrentfile/torrentfile.go
@@ -1,5 +1,12 @@
 package torrentfile
 
+import (
+	"crypto/rand"
+	"os"
+
+	"github.com/pchchv/torrent-client/p2p"
+)
+
 // Port to listen on
 const Port uint16 = 6881
 
@@ -23,4 +30,43 @@ type bencodeInfo struct {
 type bencodeTorrent struct {
 	Announce string      `bencode:"announce"`
 	Info     bencodeInfo `bencode:"info"`
+}
+
+// DownloadToFile downloads a torrent and writes it to a file
+func (t *TorrentFile) DownloadToFile(path string) error {
+	var peerID [20]byte
+	_, err := rand.Read(peerID[:])
+	if err != nil {
+		return err
+	}
+
+	peers, err := t.requestPeers(peerID, Port)
+	if err != nil {
+		return err
+	}
+
+	torrent := p2p.Torrent{
+		Peers:       peers,
+		PeerID:      peerID,
+		InfoHash:    t.InfoHash,
+		PieceHashes: t.PieceHashes,
+		PieceLength: t.PieceLength,
+		Length:      t.Length,
+		Name:        t.Name,
+	}
+	buf, err := torrent.Download()
+	if err != nil {
+		return err
+	}
+
+	outFile, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer outFile.Close()
+	_, err = outFile.Write(buf)
+	if err != nil {
+		return err
+	}
+	return nil
 }
